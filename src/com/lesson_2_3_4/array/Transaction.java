6b8a2fa54009896f1db0d281ed232cf4111d4011
arkbazh <minder.dings2z@icloud.com>
2025-08-12 14:22:25 +0400

    Fix Transaction.java

--- src/com/lesson_2_3_4/array/Transaction.java
+++ src/com/lesson_2_3_4/array/Transaction.java
@@ -3,40 +3,43 @@ package src.com.lesson_2_3_4.array;
 import java.util.Arrays;
 
 public class Transaction {
-   public static int[] reverse(int[] transaction) {
-      if (transaction == null) {
-         return null;
-      }
-      if (transaction.length == 0) {
-         return new int[0];
-      }
-      int length = transaction.length;
-      int[] reverse = Arrays.copyOf(transaction, length);
-      for (int i = 0, j = length - 1; i < length; i++, j--) {
-         reverse[i] = transaction[j];
-      }
-      return reverse;
-   }
+    public static int[] reverse(int[] transaction) {
+        if (transaction == null) {
+            return null;
+        }
+        if (transaction.length == 0) {
+            return new int[0];
+        }
+        int length = transaction.length;
+        int[] reverse = Arrays.copyOf(transaction, length);
+        for (int i = 0, j = length - 1; i < length; i++, j--) {
+            reverse[i] = transaction[j];
+        }
+        return reverse;
+    }
 
-   private static void printTransactions(int[] transaction) {
-      System.out.print("Исходные транзакции: ");
-      System.out.println(transaction == null ? "Транзакций нет" : Arrays.toString(transaction));
+    private static void printTransactions(int[] transaction) {
+        System.out.print("Исходные транзакции: ");
+        System.out.println(transaction == null ? "Транзакций нет" : Arrays.toString(transaction));
 
-      System.out.print("В обратном порядке: ");
-      System.out.println(transaction == null ? "Транзакций нет" : Arrays.toString(reverse(transaction)));
-   }
+        System.out.print("В обратном порядке: ");
+        System.out.println(transaction == null ? "Транзакций нет" : Arrays.toString(reverse(transaction)));
+    }
 
-   public static void main(String[] args) {
-      int[] emptyTransactions = new int[0];
-      int[] nullTransactions = null;
-      int[] defaultTransactions = new int[5];
-      int[] sampleTransactions = new int[] { 6, 8, 9, 1 };
-      int[] anotherTransactions = new int[] { 13, 8, 5, 3, 2, 1, 1 };
+    public static void main(String[] args) {
+        int[] emptyTransactions = new int[0];
+        printTransactions(emptyTransactions);
 
-      printTransactions(emptyTransactions);
-      printTransactions(nullTransactions);
-      printTransactions(defaultTransactions);
-      printTransactions(sampleTransactions);
-      printTransactions(anotherTransactions);
-   }
+        int[] nullTransactions = null;
+        printTransactions(nullTransactions);
+
+        int[] defaultTransactions = new int[5];
+        printTransactions(defaultTransactions);
+
+        int[] sampleTransactions = new int[]{6, 8, 9, 1};
+        printTransactions(sampleTransactions);
+
+        int[] anotherTransactions = new int[]{13, 8, 5, 3, 2, 1, 1};
+        printTransactions(anotherTransactions);
+    }
 }
