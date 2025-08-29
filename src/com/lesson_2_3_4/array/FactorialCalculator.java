14e308450e9d3c006cb8c2ca0c8229829c5ede1f
arkbazh <minder.dings2z@icloud.com>
2025-08-29 21:33:02 +0400

    Fix FactorialCalculator

--- src/com/lesson_2_3_4/array/FactorialCalculator.java
+++ src/com/lesson_2_3_4/array/FactorialCalculator.java
@@ -2,25 +2,23 @@ package src.com.lesson_2_3_4.array;
 
 public class FactorialCalculator {
     public static void main(String[] args) {
-        int[] numbers;
-
-        numbers = new int[]{};
-        print(numbers, calc(numbers));
+        int[] numbers = {};
+        printFactorials(numbers, calc(numbers));
 
         numbers = null;
-        print(numbers, calc(numbers));
+        printFactorials(numbers, calc(numbers));
 
         numbers = new int[]{8, 0, 9};
-        print(numbers, calc(numbers));
+        printFactorials(numbers, calc(numbers));
 
         numbers = new int[]{-3, 1, 7, 13};
-        print(numbers, calc(numbers));
+        printFactorials(numbers, calc(numbers));
 
         numbers = new int[]{-22, -0};
-        print(numbers, calc(numbers));
+        printFactorials(numbers, calc(numbers));
     }
 
-    public static int[] calc(int... numbers) {
+    private static int[] calc(int... numbers) {
         if (numbers == null) {
             System.out.println("Ошибка: массив null");
             return null;
@@ -30,20 +28,23 @@ public class FactorialCalculator {
             return numbers;
         }
 
-        int[] factorials = numbers.clone();
+        int[] factorials = new int[numbers.length];
 
         for (int i = 0; i < numbers.length; i++) {
             if (numbers[i] < 0) {
                 System.out.printf("Ошибка: факториал %d! не определен\n", factorials[i]);
                 continue;
             }
-            int fact = factorial(numbers[i]);
-            factorials[i] = fact;
+            factorials[i] = factorial(numbers[i]);
         }
         return factorials;
     }
 
-    public static void print(int[] numbers, int[] factorials) {
+    private static int factorial(int num) {
+        return (num == 0 || num == 1) ? 1 : num * factorial(num - 1);
+    }
+
+    private static void printFactorials(int[] numbers, int[] factorials) {
         if (numbers == null) {
             System.out.println("Ошибка: исходный массив null");
             return;
@@ -55,22 +56,19 @@ public class FactorialCalculator {
 
         for (int i = 0; i < factorials.length; i++) {
             if (numbers[i] >= 0) {
-                System.out.println(expr(numbers[i], factorials[i]));
+                System.out.println(formatFactorialExpr(numbers[i], factorials[i]));
             }
         }
         System.out.println();
     }
 
-    private static int factorial(int n) {
-        return (n == 0 || n == 1) ? 1 : n * factorial(n - 1);
-    }
-
-    private static String expr(int num, int fact) {
+    private static String formatFactorialExpr(int num, int fact) {
         if (num == 0 || num == 1) {
-            return num + "! = 1 = " + fact;
+            return num + "! = " + fact;
         }
 
-        StringBuilder sb = new StringBuilder().append(num).append("! = 1");
+        StringBuilder sb = new StringBuilder();
+        sb.append(num).append("! = 1");
         for (int i = 2; i <= num; i++) {
             sb.append(" * ").append(i);
         }
