46352ed0e6c79aa5c5fae3d9607336535a3319ed
arkbazh <minder.dings2z@icloud.com>
2025-09-07 11:58:14 +0400

    Add ArrayUniqueNumberFiller.java

--- src/com/lesson_2_3_4/array/ElementsRemover.java
+++ src/com/lesson_2_3_4/array/ElementsRemover.java
@@ -11,14 +11,6 @@ public class ElementsRemover {
         run(14);
     }
 
-    private static float[] generateRandomFloatArray() {
-        float[] array = new float[15];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = ThreadLocalRandom.current().nextFloat(0, 1);
-        }
-        return array;
-    }
-
     private static void run(int index) {
         final float[] original = generateRandomFloatArray();
         if (index < 0 || index >= original.length) {
@@ -33,6 +25,13 @@ public class ElementsRemover {
         printArrays(original, modified);
     }
 
+    private static float[] generateRandomFloatArray() {
+        float[] array = new float[15];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = ThreadLocalRandom.current().nextFloat(0, 1);
+        }
+        return array;
+    }
 
     private static float[] removeAboveThresholdAtIndex(float[] original, int index) {
         float[] modified = Arrays.copyOf(original, original.length);
@@ -47,14 +46,14 @@ public class ElementsRemover {
 
     private static void printArrays(float[] original, float[] modified) {
         System.out.println("Исходный массив");
-        printArraysAtTwoStrings(original);
+        printArrayAtTwoStrings(original);
 
         System.out.println("Измененный массив");
-        printArraysAtTwoStrings(modified);
+        printArrayAtTwoStrings(modified);
         System.out.printf("%n");
     }
 
-    private static void printArraysAtTwoStrings(float[] array) {
+    private static void printArrayAtTwoStrings(float[] array) {
         for (int i = 0; i < array.length; i++) {
             System.out.printf("%.3f ", array[i]);
             if (i == 7) {
